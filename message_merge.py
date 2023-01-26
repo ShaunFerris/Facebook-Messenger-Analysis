cf776be308418082dab13dc34cbe929eb77ff6e8
Shaun Ferris <slb.ferris46@gmail.com>
2023-01-26 14:24:07 +0100

    Re-write to allow import of message merge into other modules

--- message_merge.py
+++ message_merge.py
@@ -1,33 +1,48 @@
-'''Small utility script for merging multiple .json files from the same messenger conversation into one for analysis
-by message analysis.py'''
+'''Small utility script for merging multiple .json files from the same messenger conversation into one for analysis.
+The script can be run stand-alone to prompt the user through the process of merging files, or the functionality
+can be imported, allowing automation or other applications.'''
 
 import json
+from time import sleep
 
-print('how many files do you want to merge?')
-num_files_to_merge = int(input('>>'))
-files_to_merge = {n: 0 for n in range(1, num_files_to_merge + 1)}
-for n in files_to_merge:
-    print('Input file...')
-    files_to_merge[n] = input('>>')
+def fb_chat_merge(files_to_merge: list, outfile_name: str='unnamed_merged_messages'):
+    '''The function to import to apply the file merge functionality
+    in other scripts. Takes a list containing the paths to files,
+    and a name to give the completed file.
+    
+    Returns one merged .json file, in the schema used by facebook.'''
 
-#add latest timestamp to data so they can be stitched in the right order
-for n, f in files_to_merge.items():
-    file = json.load(open(f))
-    files_to_merge[n] =(f, file['messages'][0]['timestamp_ms'])
-#Set the order to stitch files by index in a list
-files_to_merge = sorted(files_to_merge.values(), key=lambda x:x[1], reverse=True)
+    files_to_merge = {n + 1: f for n, f in enumerate(files_to_merge)}
+    #Add latest timestamp to data so they can be stitched in the right order
+    for n, f in files_to_merge.items():
+        file = json.load(open(f))
+        files_to_merge[n] = (f, file['messages'][0]['timestamp_ms'])
+    #Set the order to stitch files by index in a list
+    print(files_to_merge)
+    files_to_merge = sorted(files_to_merge.values(), key=lambda x:x[1], reverse=True)
+    print(files_to_merge)
+    #Set latest fragment as top of file
+    output = json.load(open(files_to_merge[0][0]))
+    count = 0
+    for tup in files_to_merge:
+        count += 1
+        if count == 1:
+            continue
+        data = json.load(open(tup[0]))
+        output['messages'].extend(data['messages'])
 
-output = json.load(open(files_to_merge[0][0]))
-count = 0
-for tup in files_to_merge:
-    count += 1
-    if count == 1:
-        continue
-    data = json.load(open(tup[0]))
-    output['messages'].extend(data['messages'])
+    with open(outfile_name, 'x') as outfile:
+        json.dump(output, outfile, indent=2)
 
-print('Enter name for merged file')
-out_name = input('>>')
-
-with open(out_name, 'x') as outfile:
-    json.dump(output, outfile, indent=2)
+if __name__ == '__main__':
+    file_list = []
+    print('How many files would you like to merge?')
+    num_files_to_merge = input('>> ')
+    for i in range(1, num_files_to_merge + 1):
+        print('Enter path to file: ')
+        file_list.append(input('>> '))
+    print('What would you like the merged file to be called?')
+    outfile_name = input('>> ')
+    fb_chat_merge(file_list, outfile_name)
+    sleep(0.5)
+    print('File created.')
