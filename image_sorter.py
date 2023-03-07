7a06cac07238db5f1f06cf71f9f38f10d2895a6f
Shaun Ferris <slb.ferris46@gmail.com>
2023-03-07 14:00:05 +0000

    Docstring for main sorting algorithm

--- image_sorter.py
+++ image_sorter.py
@@ -9,14 +9,14 @@ from facebookchat import FacebookChat as FB
 
 test_chat = FB('/home/shaun/Documents/coding-projects/Facebook Data Analysis/essie_merged_messagesV2.json')
 
-def get_photo_info(chat_json):
+def get_image_info(chat_json):
     '''Takes as an arg the path to the chat json of interest.
     Initializes the chat as an instance of the FacebookChat class and
     uses the photos_by_sender method to organize the photo file names by
     who sent them and when.'''
 
     chat_object = FB(chat_json)
-    return chat_object.photos_by_sender()
+    return chat_object.images_by_sender()
 
 def make_file_structure(start_year, end_year, title):
     '''Creates a file structure on the users desktop. The outer folder is 
@@ -27,12 +27,12 @@ def make_file_structure(start_year, end_year, title):
     
     #Get path to desktop and create photos folder if it doesn't already exist
     desktop_path = os.path.join(os.path.expanduser('~'), 'Desktop')
-    photos_path = os.path.join(desktop_path, f'photos_from_facebook_chat_{title}')
-    if not os.path.exists(photos_path):
-        os.mkdir(photos_path)
+    images_path = os.path.join(desktop_path, f'photos_from_facebook_chat_{title}')
+    if not os.path.exists(images_path):
+        os.mkdir(images_path)
     #Make a folder for each year in the range
     for year in range(start_year, end_year + 1):
-        year_folder = os.path.join(photos_path, str(year))
+        year_folder = os.path.join(images_path, str(year))
         if not os.path.exists(year_folder):
             os.mkdir(year_folder)
         #Create monthly photos folders for each year
@@ -42,4 +42,25 @@ def make_file_structure(start_year, end_year, title):
             if not os.path.exists(month_folder):
                 os.mkdir(month_folder)
     
+def sort_pictures(unsorted, destination_file_struct, images_by_sender):
+    '''Moves the images from the 'photos' folder in the chat data directory
+    to a newly created file structure organising them by month of the year sent
+    and sender.
     
+    Args: 
+        unsorted: Path to 'photos' folder.
+
+        destination_file_struct: Path to the top-level directory of a file structure
+        made with the make_file_structure function.
+        
+        images_by_sender: The dictionary made by the images_by_sender function of a
+        FacebookChat class instance.'''
+    
+    for sender_name, images in images_by_sender.items():
+        for image in images:
+            sent_date = image.keys()[0]
+            file_name = image.values()[0]
+            year, month, day = sent_date.split('-')
+            pass
+
+print(test_chat.images_by_sender())
